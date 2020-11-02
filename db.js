0eb12590b15e57ab140d1a2919328d4b280e0b65
dmytrohryb <dmytro.hryb@nure.ua>
2020-11-02 21:58:26 +0200

    test 2

--- db.js
+++ db.js
@@ -15,26 +15,6 @@ Bike.belongsTo(Type)
 Bike.hasMany(Order)
 Order.belongsTo(Bike)
 
-async function initData (){
-
-    await Type.sync({ force: true })
-    await Bike.sync({ force: true })
-    await Order.sync({ force: true })
-    await Type.create({name: "track"})
-    await Type.create({name: "mountain"})
-    await Type.create({name: "pitbike"})
-    //await Bike.create({name: "Bike 1", price: 12, available: true, typeId: 1})
-    //await Bike.create({name: "Bike 2", price: 11, available: true, typeId: 2})
-    //await Bike.create({name: "Bike 3", price: 11, available: false, typeId: 2})
-    //await Bike.create({name: "Bike 4", price: 11, available: false, typeId: 2})
-    //await Order.create({quantity: 12, bikeId: 1})
-    //await Order.create({quantity: 12, bikeId: 2})
-
-
-}
-
-initData()
-
 module.exports = {
     sequelize: sequelize,
     bike: Bike,
